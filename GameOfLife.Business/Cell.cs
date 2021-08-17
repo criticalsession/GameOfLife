262e7d3613d5a75e0dc84a19c75dee9735575e2e
Critical Session <criticalsession@protonmail.ch>
2021-08-17 09:18:37 +0200

    Add project files.

---
+++ GameOfLife.Business/Cell.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GameOfLife.Business
+{
+    public class Cell
+    {
+
+    }
+}
---
+++ GameOfLife.UnitTests/CellTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace GameOfLife.UnitTests
+{
+    public class CellTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
